4f27fec2c373bcb7b7a884771b63168916e5b6cc
Nefas <tiv.crb@gmail.com>
2026-03-23 23:53:33 +0100

    Update macro usage

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,29 @@ macro_rules! typed_get {
     ($hash:ident, $idx:literal, $rtype:path, $expr:expr) => {
         match &**$hash.get(&$idx.to_bigint().unwrap()).unwrap() {
             $rtype(value) => $expr(value),
-            v => panic!("Expected {} : {:?}", stringify!($rytpe), v),
+            v => panic!("Expected {}, got {:?} for {} at index {}",
+                        stringify!($rtype), v, stringify!($hash), $idx),
         }
     };
     ($hash:ident, $idx:literal, $rtype:path) => {
         match &**$hash.get(&$idx.to_bigint().unwrap()).unwrap() {
             $rtype(value) => value,
-            v => panic!("Expected {} : {:?}", stringify!($rytpe), v),
+            v => panic!("Expected {}, got {:?} for {} at index {}",
+                        stringify!($rtype), v, stringify!($hash), $idx),
         }
     };
     ($hash:ident, $idx:expr, $rtype:path) => {
         match &**$hash.get(&$idx).unwrap() {
             $rtype(value) => value,
-            v => panic!("Expected {} : {:?}", stringify!($rytpe), v),
+            v => panic!("Expected {}, got {:?} for {} at index {}",
+                        stringify!($rtype), v, stringify!($hash), $idx),
         }
     };
     ($hash:ident, $idx:expr, $rtype:path, $expr:expr) => {
         match &**$hash.get(&$idx).unwrap() {
             $rtype(value) => $expr(value),
-            v => panic!("Expected {} : {:?}", stringify!($rytpe), v),
+            v => panic!("Expected {}, got {:?} for {} at index {}",
+                        stringify!($rtype), v, stringify!($hash), $idx),
         }
     };
 }
@@ -208,35 +212,110 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let file_data = find_and_read_file(data, String::from("replay.details"), &hashtable,
                                        &crypttable, &blocktable, &mpqheader).unwrap();
     let (_left_over, (parsed, _)) = sc2::parser::parse_struct(&file_data, 0).unwrap();
-    // FIXME make a macro
     println!("{}", parsed);
-    let details = match parsed {
-        sc2::structs::DataType::HashMap(map) => map,
-        _ => panic!("Expected details to be a hashmap"),
-    };
-    let players = match &**details.get(&BigInt::ZERO).unwrap() {
-        sc2::structs::DataType::Array(ary) => ary,
-        _ => panic!("Expected to be an array"),
-    };
+    // FIXME : add version dependant things
+    let details =  typed_expect!(parsed, sc2::structs::DataType::HashMap);
+    println!("{{");
+
+    let map_name = typed_get!(details, 1, sc2::structs::DataType::Blob, decode_blob);
+    println!("    map name : {}", map_name);
+
+    let difficulty = typed_get!(details, 2, sc2::structs::DataType::Blob, decode_blob);
+    println!("    difficulty : \"{}\"", difficulty);
+
+    let thumbnail = typed_get!(details, 3, sc2::structs::DataType::HashMap);
+    let thumbnail = typed_get!(thumbnail, 0, sc2::structs::DataType::Blob, decode_blob);
+    println!("    thumbnail : \"{}\"", thumbnail);
+
+    let blizz_map = typed_get!(details, 4, sc2::structs::DataType::UInt8);
+    println!("    blizzard map : {}", blizz_map);
+
+    let file_time = typed_get!(details, 5, sc2::structs::DataType::VInt);
+    println!("    file time : {}", file_time);
+
+    let utc_adjustment = typed_get!(details, 6, sc2::structs::DataType::VInt);
+    println!("    UTC adjustment : {}", utc_adjustment);
+
+    let description = typed_get!(details, 7, sc2::structs::DataType::Blob, decode_blob);
+    println!("    description : {}", description);
+
+    let image_file_path = typed_get!(details, 8, sc2::structs::DataType::Blob, decode_blob);
+    println!("    image file path : {}", image_file_path);
+
+    let map_file_name = typed_get!(details, 9, sc2::structs::DataType::Blob, decode_blob);
+    println!("    map file name : {}", map_file_name);
+
+    let cache_handle = typed_get!(details, 10, sc2::structs::DataType::Array);
+    let cache_handle = cache_handle.iter().map(
+        |v| {
+            let bytes = typed_expect!(&**v, sc2::structs::DataType::Blob);
+            let mut server = decode_utf8(&bytes[4..8]).unwrap().trim_matches(|c| c == '\x00' || c == ' ').to_lowercase();
+            let mut res: Vec<u8> = Vec::new();
+            for e in &bytes[8..] {
+                let s = format!("{:02x}", e);
+                res.push(s.bytes().nth(0).unwrap());
+                res.push(s.bytes().nth(1).or(Some(0)).unwrap());
+            }
+            let hash = decode_utf8(&res).unwrap();
+            let type_ = decode_utf8(&bytes[0..4]).unwrap();
+            let mut scheme = "https";
+            let mut domain = "classic.blizzard.com";
+
+            if server == "sea" {
+                server = "us".to_string();
+            } else if server == "cn" {
+                scheme = "http";
+                domain = "battlenet.com.cn";
+            };
+
+            format!("{}://{}-s2-depot.{}/{}.{}", scheme, server, domain, hash, type_)
+
+        }
+    ).collect::<Vec<String>>();
+    println!("    cache handle : {:?}", cache_handle);
+
+    let mini_save = typed_get!(details, 11, sc2::structs::DataType::UInt8);
+    println!("    mini save : {}", mini_save);
+
+    let game_speed = typed_get!(details, 12, sc2::structs::DataType::VInt);
+    println!("    game speed : {}", game_speed);
+
+    let default_difficulty = typed_get!(details, 13, sc2::structs::DataType::VInt);
+    println!("    default difficulty : {}", default_difficulty);
+
+    let players =  typed_get!(details, BigInt::ZERO, sc2::structs::DataType::Array);
     for player in players {
         let player =   typed_expect!(&**player, sc2::structs::DataType::HashMap);
         let name =     typed_get!(player, BigInt::ZERO, sc2::structs::DataType::Blob,    decode_blob);
+        let bnet =     typed_get!(player, 1,            sc2::structs::DataType::HashMap,
+                                  |hm: &HashMap<BigInt, Box<sc2::structs::DataType>>| (
+                                      typed_get!(hm, 0, sc2::structs::DataType::VInt).clone(),
+                                      typed_get!(hm, 1, sc2::structs::DataType::UInt32).clone(),
+                                      typed_get!(hm, 2, sc2::structs::DataType::VInt).clone(),
+                                      // Documented but never available
+                                      // typed_get!(hm, 3, sc2::structs::DataType::VInt).clone(),
+                                      typed_get!(hm, 4, sc2::structs::DataType::VInt).clone(),
+                                  )
+        );
         let race =     typed_get!(player, 2,            sc2::structs::DataType::Blob,    decode_blob);
         let color =    typed_get!(player, 3,            sc2::structs::DataType::HashMap, parse_color_hashmap);
         let control =  typed_get!(player, 4,            sc2::structs::DataType::VInt);
+        let team =     typed_get!(player, 5,            sc2::structs::DataType::VInt);
         let handicap = typed_get!(player, 6,            sc2::structs::DataType::VInt);
         let observe =  typed_get!(player, 7,            sc2::structs::DataType::VInt);
         let result =   typed_get!(player, 8,            sc2::structs::DataType::VInt);
 
-        println!("{{
-    name: {},
-    race: {},
-    colour: {{ a: {}, r: {}, g: {}, b: {} }},
-    control: {},
-    handicap: {},
-    observe: {},
-    result: {};
-}}",
+        println!("    {{
+        name: {},
+        race: {},
+        colour: {{ a: {}, r: {}, g: {}, b: {} }},
+        control: {},
+        handicap: {},
+        observe: {},
+        result: {};
+        bnet: {{ region : {}, program_id : {}, subregion : {}, uid : {} }};
+        team: {};
+    }}",
                  name,
                  race,
                  color[&0], color[&1], color[&2], color[&3],
@@ -244,7 +323,10 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
                  handicap,
                  observe,
                  result,
+                 bnet.0, bnet.1, bnet.2, bnet.3,
+                 team,
         )
     }
+    println!("{{");
     Ok(())
 }
